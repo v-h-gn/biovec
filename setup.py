1b0e117cf59635599e9bafcf4bda8dff98f3dba7
kyu999 <kyukokkyou999@gmail.com>
2018-11-21 10:58:16 +0900

    update version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ REQUIRED_PACKAGES = [
 ]
 
 setup(name='biovec',
-      version='0.2.3',
+      version='0.2.4',
       license='MIT',
       description=description,
       long_description=description,
